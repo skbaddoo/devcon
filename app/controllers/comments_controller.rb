20d5342da119eb9983b6ceac079f0d3df4fd03d9
@ganyobinii <niibaddoo@gmail.com>
2018-08-27 03:06:19 -0400

    making associations for post ans categories

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
 
   def destroy
 	@post = Post.find(params[:post_id])
-	@comment = @post.comment.find(params[:id])
+	@comment = @post.comments.find(params[:id])
 	@comment.destroy
 	redirect_to post_path(@post)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+	belongs_to :category
 	has_many :comments, dependent: :destroy
 	  #dependent: :destroy means the comments related
 	  # to the specific post in mention get deleted if the post does. 
+	validates :title, :content, :category_id, presence: true
 end
