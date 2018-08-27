f7553ac82ea160769b996107e73914cfd77ef282
@ganyobinii <niibaddoo@gmail.com>
2018-08-27 02:22:29 -0400

    added associations for comments and posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+
+  def create
+	@post = Post.find(params[:post_id])
+	  @comment = @post.comments.create(params[:comment].permit(:name, :content))
+	  redirect_to post_path(@post)
+  end
+
+  def destroy
+	@post = Post.find(params[:post_id])
+	@comment = @post.comment.find(params[:id])
+	@comment.destroy
+	redirect_to post_path(@post)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+	has_many :comments, dependent: :destroy
+	  #dependent: :destroy means the comments related
+	  # to the specific post in mention get deleted if the post does. 
 end
